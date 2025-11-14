fc89ba19675f7e17f91fcbe0c00b6fae49c6abaf
Swetha <buddiswetha12@gmail.com>
2025-11-14 12:03:22 +0530

    Intruction of Methods

---
+++ src/Locators.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.findElement(By.id("inputUsername")).sendKeys("rahul");
+		driver.findElement(By.name("inputPassword")).sendKeys("hello123");
+		driver.findElement(By.className("signInBtn")).click();
+		}
+
+}
--- src/MethodsDemo.java
+++ src/MethodsDemo.java
@@ -3,7 +3,22 @@ public class MethodsDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		MethodsDemo d = new MethodsDemo();
+		String name = d.getData();
+		System.out.println(name);
+		MethodsDemo2 d1 = new MethodsDemo2();
+		d1.getUserData();
+		getData2();
+	}
+	
+	public String getData() {
+		System.out.println("Hello World!");
+		return "rahul shetty";
+	}
+	
+	public static String getData2() {
+		System.out.println("Hello World!");
+		return "rahul shetty";
 	}
 
 }
