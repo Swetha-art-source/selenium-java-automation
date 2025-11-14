3d3240d74dbfb592aea04ee9ea3f7f5d8d3be768
Swetha <buddiswetha12@gmail.com>
2025-11-14 11:21:10 +0530

    Core Java Brushup

--- src/CoreJavaBrushUp3.java
+++ src/CoreJavaBrushUp3.java
@@ -3,12 +3,21 @@ public class CoreJavaBrushUp3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s = "Rahul Shetty Academy";
+		
 		String s1 = "Rahul Shetty Academy";
 		String s5 = "Hello";
 		
 		String s2 = new String("Welcom");
 		String s3 = new String("Welcome");
+		
+		String s = "Rahul Shetty Academy";
+		String[] splittedString = s.split("Shetty");
+		System.out.println(splittedString[0]);
+		System.out.println(splittedString[1]);
+		System.out.println(splittedString[1].trim());
+		for(int i=0; i<s.length(); i++) {
+			System.out.println(s.charAt(i));
+		}
 
 	}
 
