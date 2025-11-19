3e4c256fbf17c59abc4a8b6e9c7e61e96c38b9fd
Swetha <buddiswetha12@gmail.com>
2025-11-19 15:22:56 +0530

    Explore new locators

--- src/Locators.java
+++ src/Locators.java
@@ -1,3 +1,5 @@
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,10 +9,20 @@ public class Locators {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.findElement(By.id("inputUsername")).sendKeys("rahul");
 		driver.findElement(By.name("inputPassword")).sendKeys("hello123");
 		driver.findElement(By.className("signInBtn")).click();
+		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+		driver.findElement(By.linkText("Forgot your password?")).click();
+		driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("Jhon");
+		driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("jhon@rsa.com");
+		driver.findElement(By.cssSelector("input[placeholder='Email']")).clear();
+		driver.findElement(By.cssSelector("input[type='text']:nth-child(3)")).sendKeys("jhon@gmail.com");
+		driver.findElement(By.cssSelector("//form/input[3]")).sendKeys("9876533223");
+		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+		driver.findElement(By.cssSelector("form p")).getText();
 		}
 
 }
